bd85943c887dff2ab934a7ca24455ff1189c7fb0
Mats Huesmann <huesmann.mats@gmail.com>
2023-10-09 13:32:19 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import * as TerraconnectUI from 'terraconnect-ui';
 import { Value, createState } from 'terraconnect-state';
 
-let test = createState(0);
-
 const App: TerraconnectUI.Component = () => {
   return (
     <>
-      <div onClick={() => test[Value]++}>Test 3 {test}</div>
+
     </>
   );
 }
