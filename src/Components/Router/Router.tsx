0c2d7d86f86f3dab9c68470b75174c1435d73121
Mats Huesmann <huesmann.mats@gmail.com>
2024-01-14 18:37:09 +0100

    fix router history duplication

--- src/Components/Router/Router.tsx
+++ src/Components/Router/Router.tsx
@@ -10,33 +10,43 @@ type RouterProps = {
 
 type filter<T> = (predicate: (value: T, index: number, array: T[]) => boolean) => State<T[]>;
 
-export let path = createState((window.location.pathname + (window.location.pathname.endsWith("/") ? "" : "/")).replace("/index.html/", "/"));
+export let path = createState(decodeURI(window.location.pathname + (window.location.pathname.endsWith("/") ? "" : "/")).replace("/index.html/", "/"));
 
 export function assign(url: string) {
   url = url.replace(window.location.origin, "");
   window.history.pushState(url, "", url);
-  path[Value] = url + (url.endsWith("/") ? "" : "/");
+  path[Value] = decodeURI(url + (url.endsWith("/") ? "" : "/"));
+}
+
+export function back() {
+  window.history.back();
 }
 
 window.addEventListener('popstate', (event) => {
   let state: string = event.state ?? "/";
   state += (state.endsWith("/") ? "" : "/");
   state = state.replace("/index.html/", "/");
-  path[Value] = state;
+  path[Value] = decodeURI(state);
 });
 
 const Router: TerraconnectUI.ComponentFN<RouterProps> = ({ children }) => {
+  children = flattenChildren(children as unknown as State<Array<TerraconnectUI.HTMLComponent<any>>>) as unknown as typeof children;
   // let filter = children.filter[Value] as unknown as filter<State<HTMLComponent<RouteProps>>>; WTF WHY
   let filter = children.filter[Value] as unknown as filter<State<{ component: string | TerraconnectUI.Component, props: State<RouteProps> }>>;
   // let routes = filter((route) => route.component[Value] == Route && route.props.path[Value] == path[Value]) as State<Array<unknown>> as State<Array<HTMLComponent<RouteProps>>>;
   let notFound = filter((route) => route.component[Value] == RouteNotFound) as State<Array<unknown>> as State<Array<HTMLComponent<RouteProps>>>;
-  let routes = createState((children: Array<HTMLComponent<RouteProps>>, path) => children.filter((route) => route.component == Route && route.props.path[Value] == path), [children as any, path])
+  let routes = createState((children: Array<HTMLComponent<RouteProps>>, path) => children.filter((route) => route.component == Route && route.props.path[Value] == path), [children as any, path]);
+  console.log(children, path);
   let route = createState((routes, notFound) => routes.length != 0 ? routes : notFound, [routes, notFound]);
-  let assignRoute = (route: TerraconnectUI.HTMLComponent<RouteProps>) => route.querySelectorAll('a').forEach((element) => element.addEventListener('click', (event) => {
+  let clickRoute = (event: MouseEvent) => {
     event.stopPropagation();
     event.preventDefault();
-    assign(element.href);
-  }));
+    assign((event.target as HTMLAnchorElement).href);
+  };
+  let assignRoute = (route: TerraconnectUI.HTMLComponent<RouteProps>) => route.querySelectorAll('a').forEach((element) => {
+    element.removeEventListener('click', clickRoute);
+    element.addEventListener('click', clickRoute);
+  });
   route[Modified].on((routes) => routes.forEach(assignRoute));
   route[Value].forEach(assignRoute);
   return (
@@ -44,6 +54,15 @@ const Router: TerraconnectUI.ComponentFN<RouterProps> = ({ children }) => {
       {route}
     </>
   );
+}
+function flattenChildren(children: State<Array<TerraconnectUI.HTMLComponent<any>>>) {
+  return (children.reduce[Value]((a, b, i) => {
+    if (Array.isArray(b[Value]))
+      a.push(...flattenChildren(b)[Value]);
+    else
+      a.push(b[Value]);
+    return a;
+  }, [] as Array<TerraconnectUI.HTMLComponent<any>>) as State<any>).filter[Value]((_: any) => _ != null) as typeof children;
 }
 
 export default Router as unknown as TerraconnectUI.Component<RouterProps>;
