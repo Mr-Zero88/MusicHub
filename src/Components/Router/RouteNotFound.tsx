97d262caeb281c9be2ec8de688d644fdeb0e0ed0
Mats Huesmann <huesmann.mats@gmail.com>
2024-07-17 15:10:07 +0200

    Fix NotFound Route Error

--- src/Components/Router/RouteNotFound.tsx
+++ src/Components/Router/RouteNotFound.tsx
@@ -1,6 +1,9 @@
+import { createState, PreserveState } from 'terraconnect-state';
 import * as TerraconnectUI from 'terraconnect-ui';
 
 const RouteNotFound: TerraconnectUI.ComponentFN = function ({ children }) {
+  (this as any).visible = createState(false);
+  (this as any).visible[PreserveState] = true;
   return (
     <>
       {children}
--- src/Components/Router/Router.tsx
+++ src/Components/Router/Router.tsx
@@ -30,13 +30,11 @@ window.addEventListener('popstate', (event) => {
 });
 
 const Router: TerraconnectUI.ComponentFN<RouterProps> = ({ children }) => {
-  children = flattenChildren(children as unknown as State<Array<TerraconnectUI.HTMLComponent<any>>>) as unknown as typeof children;
-  // let filter = children.filter[Value] as unknown as filter<State<HTMLComponent<RouteProps>>>; WTF WHY
-  let filter = children.filter[Value] as unknown as filter<State<{ component: string | TerraconnectUI.Component, props: State<RouteProps<any, any>> }>>;
-  // let routes = filter((route) => route.component[Value] == Route && route.props.path[Value] == path[Value]) as State<Array<unknown>> as State<Array<HTMLComponent<RouteProps>>>;
-  let notFound = filter((route) => route.component[Value] == RouteNotFound) as State<Array<unknown>> as State<Array<HTMLComponent<RouteProps<any, any>>>>;
-  let routes = createState((children: Array<HTMLComponent<RouteProps<any, any>>>, path) => children.filter((route) => route.component == Route && route.props.path[Value] == path), [children as any, path]);
-  // console.log(children, path);
+  let flattenedChildren = flattenChildren(children as unknown as State<Array<TerraconnectUI.HTMLComponent<any>>>);
+  // let filter = flattenedChildren.filter[Value] as unknown as filter<State<{ component: string | TerraconnectUI.Component, props: State<RouteProps<any, any>> }>>;
+  // let notFound = filter((route) => route.component[Value] == RouteNotFound) as State<Array<unknown>> as State<Array<HTMLComponent<RouteProps<any, any>>>>;
+  let notFound = createState((children: Array<HTMLComponent<RouteProps<any, any>>>, path) => children.filter((route) => route.component == RouteNotFound), [flattenedChildren as any, path]);
+  let routes = createState((children: Array<HTMLComponent<RouteProps<any, any>>>, path) => children.filter((route) => route.component == Route && route.props.path[Value] == path), [flattenedChildren as any, path]);
   let route = createState((routes, notFound) => routes.length != 0 ? routes : notFound, [routes, notFound]);
   route[Modified].on((newRoute, oldRoute) => {
     (oldRoute[0] as any).visible[Value] = false;
@@ -49,6 +47,11 @@ const Router: TerraconnectUI.ComponentFN<RouterProps> = ({ children }) => {
     </>
   );
 }
+
+function isHTMLComponent<T>(value: any): value is State<HTMLComponent<T>> {
+  return value != null;
+}
+
 function flattenChildren(children: State<Array<TerraconnectUI.HTMLComponent<any>>>) {
   return (children.reduce[Value]((a, b, i) => {
     if (Array.isArray(b[Value]))
@@ -56,7 +59,7 @@ function flattenChildren(children: State<Array<TerraconnectUI.HTMLComponent<any>
     else
       a.push(b[Value]);
     return a;
-  }, [] as Array<TerraconnectUI.HTMLComponent<any>>) as State<any>).filter[Value]((_: any) => _ != null) as typeof children;
+  }, [] as Array<TerraconnectUI.HTMLComponent<any>>)).filter[Value]<Array<TerraconnectUI.HTMLComponent<any>>>(isHTMLComponent);
 }
 
 export default Router as unknown as TerraconnectUI.Component<RouterProps>;
