2a3c03c157f40e09c9edd9d71280213a66f27d46
Mats Huesmann <huesmann.mats@gmail.com>
2024-07-17 12:42:36 +0200

    Cache clinc info data

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,15 +1,16 @@
 import { createState, Value } from "terraconnect-state";
 import { Clinic } from "./types";
 
-export const clinics = createState<Array<Clinic>>(JSON.parse(localStorage.getItem('clinic') ?? '[]'));
+export const clinics = createState<Array<Clinic>>(JSON.parse(localStorage.getItem('clinics') ?? '[]'));
 fetch(`https://api.${window.location.host.split('.')[0]}.terraconnect.de/clinics`).then(data => data.json()).then(async (data) => {
-  localStorage.setItem('clinic', JSON.stringify(data));
+  localStorage.setItem('clinics', JSON.stringify(data));
   clinics[Value] = data;
 });
 
 export function getClinic(id: number) {
-  let clinic = createState<Clinic>({ id });
+  let clinic = createState<Clinic>(JSON.parse(localStorage.getItem(`clinic/${id}`) ?? 'null') || { id });
   fetch(`https://api.${window.location.host.split('.')[0]}.terraconnect.de/clinic/${id}`).then(data => data.json()).then((data) => {
+    localStorage.setItem(`clinic/${id}`, JSON.stringify(data));
     clinic[Value] = data;
   });
   return clinic;
